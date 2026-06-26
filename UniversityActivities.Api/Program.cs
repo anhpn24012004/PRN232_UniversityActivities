ca8396be532f4ecbcf987f52c322c0eee7ac336c
Ngoc Anh <anhpn24012004@gmail.com>
2026-06-26 15:08:40 +0700

    Configure database and initial migration

--- UniversityActivities.Api/Program.cs
+++ UniversityActivities.Api/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UniversityActivities.Api.Data;
+using UniversityActivities.Api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +10,38 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyCnn")));
+
+// Add Identity
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    options.Password.RequiredLength = 6;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+// Add Authorization policies
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("CanDeleteActivity", policy =>
+        policy.RequireRole("Admin"));
+
+    options.AddPolicy("OnlyStudent", policy =>
+        policy.RequireRole("Student"));
+
+    options.AddPolicy("OnlyStaffOrAdmin", policy =>
+        policy.RequireRole("Staff", "Admin"));
+
+    options.AddPolicy("OnlyOrganizer", policy =>
+    policy.RequireRole("Organizer"));
+});
 
 var app = builder.Build();
 
@@ -16,6 +53,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
